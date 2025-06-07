fbc5687c396def2c815b8cfe4604d60fcda5134c
Sam <30577766+Samasaur1@users.noreply.github.com>
2025-06-06 17:29:03 -0700

    parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+use regex::Regex;
+
+#[derive(Parser)]
+struct Args {
+    from_dir: PathBuf,
+    to_dir: PathBuf,
+    #[arg(long, default_value_t = Regex::new(r"^..").expect("The default regex value is somehow invalid"))]
+    id_regex: Regex,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = Args::parse();
+    println!("from_dir: {:?}, to_dir: {:?}, id_regex: {:?}", x.from_dir, x.to_dir, x.id_regex);
 }
